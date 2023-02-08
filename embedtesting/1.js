0819247b2ae6e90f496147025a5b76acd242a5cc
Arch <111789310+Arch881010@users.noreply.github.com>
2023-02-08 13:15:33 -0600

    Final Fix.

--- embedtesting/1.js
+++ embedtesting/1.js
@@ -1,8 +1,12 @@
 var optcount = 1;
 var int = 0;
-function createdropdown(id,amount,names,values) {
+function createdropdown(id, amount, names, values) {
     for(let vars = 0; vars<amount+1; vars++) {
-        if(amount.length != values.length || amount.length != amount || values.length != amount) {
+        if(names.length != values.length || names.length != amount || values.length != amount) {
+            console.group("Error.")
+                console.warn("The names != values test:", names.length == values,"\nThe names != amount test:",names.length == amount, "\nThe values != amount test:", values.length == amount)
+                console.error("At least 1 error occured. Please fix based on example.")
+                console.info(`Example: "The names != values test: false", "The names != amount test: false". To fix it, you need to make sure there is how many names supplied to amount of values. `)
             break;
         }
         var opt = document.createElement("option");
