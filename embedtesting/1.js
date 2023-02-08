8b551b091c0747422dc5fd4231e8d9df1977669f
Arch <111789310+Arch881010@users.noreply.github.com>
2023-02-08 12:22:00 -0600

    update

--- embedtesting/1.js
+++ embedtesting/1.js
@@ -1,7 +1,6 @@
 var optcount = 1;
 var int = 0;
 function createdropdown(id,amount,names,values) {
-    var select = document.getElementById(id)
     for(let vars = 0; vars<amount+1; vars++) {
         if(amount.length != values.length || amount.length != amount || values.length != amount) {
             break;
@@ -10,6 +9,7 @@ function createdropdown(id,amount,names,values) {
         opt.setAttribute("id",names[int]);
         opt.setAttribute("value",values[int]);
         int++;
+        var select = document.getElementById(id)
         select.appendChild(opt);
     }
 }
