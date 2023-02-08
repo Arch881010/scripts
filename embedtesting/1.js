b71ffab14d71bf5501efadd3e23ccc438d9e6cc3
Arch <111789310+Arch881010@users.noreply.github.com>
2023-02-08 12:03:28 -0600

    psh

--- embedtesting/1.js
+++ embedtesting/1.js
@@ -1,11 +1,11 @@
 var optcount = 1;
 var int = 0;
-function createdropdown(id,amount,values,names) {
-    if(amount.length != values.length || amount.length != amount || values.length != amount) {
-        break;
-    }
+function createdropdown(id,amount,names,values) {
     var select = document.getElementById(id)
     for(let vars = 0; vars<amount+1; vars++) {
+        if(amount.length != values.length || amount.length != amount || values.length != amount) {
+            break;
+        }
         var opt = document.createElement("option");
         opt.setAttribute("id",names[int]);
         opt.setAttribute("value",values[int]);
